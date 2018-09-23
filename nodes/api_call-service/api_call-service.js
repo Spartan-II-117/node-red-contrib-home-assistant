11ac37bab26df7af706a6d6282d55320ddce6871
Jason <jason@kermitdesign.com>
2018-09-23 01:08:49 -0700

    More fixes for not ignoring custom paths in node-red. Fix for Spartan-II-117/node-red-contrib-home-assistant/issues/25

--- nodes/api_call-service/api_call-service.js
+++ nodes/api_call-service/api_call-service.js
@@ -91,5 +91,12 @@ module.exports = function(RED) {
         }
     }
 
-    RED.nodes.registerType('api-call-service', CallServiceNode);
+    RED.nodes.registerType('api-call-service', CallServiceNode, {
+        settings: {
+            apiCallServiceAdminPrefix: {
+                value: RED.settings.httpAdminRoot,
+                exportable: true
+            }
+        }
+    });
 };
